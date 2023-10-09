6ca04f3887b7175f698553aa301f11c3a8f1740e
Dominion Ikpeawujor <51343512+dominionikpeawujor@users.noreply.github.com>
2023-10-09 22:05:49 +0100

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints single digits from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    for (int n = 0; n <10; n++)
+    {
+      putchar(n);
+    }
+  putchar('\n');
+  return (0);
+}
