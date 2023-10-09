fb89feca77db3fc7b662bdcff094dd57160eb263
Dominion Ikpeawujor <51343512+dominionikpeawujor@users.noreply.github.com>
2023-10-09 22:00:09 +0100

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints lowercase alphabets followed by a newline
+ * It skips q and e while printing
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+      if (letter == 'q' || letter == 'e')
+      {
+        continue;
+      }
+    putchar(letter);
+	}
+	putchar('\n');
+	
+	return (0);
+}
