fe2d5b517e0ebdf94436b11f729c2c9ffd9fedfd
Dominion Ikpeawujor <51343512+dominionikpeawujor@users.noreply.github.com>
2023-10-09 23:17:54 +0100

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of single-digit numbers,
+ * separated by a comma and space, in ascending order.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = 0; num2 < 10; num2++)
+      {
+        if (num1 >1)
+        {
+          if (num2 > num1)
+          {
+            continue;
+          }
+          else if (num1 == num2)
+          {
+            continue;
+          }
+          else if (num1 > 9 )
+          {
+            continue;
+          }
+        }
+        putchar(num1);
+        putchar(num2)
+      }
+	}
+
+	putchar('\n');
+
+	return (0);
+}
