9663f02060e33b438f48c55a960ac90427313ef0
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-10-09 14:21:24 -0700

    Update 5 and 6

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,12 +9,12 @@
  */
 int main(void)
 {
-    int n;
-    
-    for (n = 0; n < 10; n++)
-    {
-        putchar('0' + n);
-    }
-    putchar('\n');
-    return (0);
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar('0' + n);
+	}
+	putchar('\n');
+	return (0);
 }
