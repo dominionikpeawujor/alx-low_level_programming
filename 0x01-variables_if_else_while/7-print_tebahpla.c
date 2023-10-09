21a2283b46989aa66db98faf05c3c77accfa8851
Dominion Ikpeawujor <51343512+dominionikpeawujor@users.noreply.github.com>
2023-10-09 22:23:27 +0100

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints lowercase alphabets followed by a newline
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	
+	return (0);
+}
