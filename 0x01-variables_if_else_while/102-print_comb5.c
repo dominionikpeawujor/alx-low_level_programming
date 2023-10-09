04a3b0e8687ec71d93db8e3507d2a5c6e87bc25f
Dominion Ikpeawujor <51343512+dominionikpeawujor@users.noreply.github.com>
2023-10-10 00:07:58 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,17 +16,17 @@ int main(void)
 	for (num1 = 0; num1 <= 99; num1++)
 	{
 		for (num2 = num1; num2 <= 99; num2++)
-	 	{
-	 		putchar((num1 / 10) + '0');
-	 		putchar((num1 % 10) + '0');
-	 		putchar(' ');
-	 		putchar((num2 / 10) + '0');
-	 		putchar((num2 % 10) + '0');
-	 		if (num1 != 99 || num2 != 99)
-	 		{
-	 			putchar(',');
-	 			putchar(' ');
-	 		}
+		{
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+			if (num1 != 99 || num2 != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
