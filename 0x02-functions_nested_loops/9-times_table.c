a2de919978e74db0c1f2d003fadd84a1d9b0a1c9
Dominion Ikpeawujor <51343512+dominionikpeawujor@users.noreply.github.com>
2023-10-11 00:53:08 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,12 +22,13 @@ void times_table(void)
 
             if (column > 0) {
                 _putchar(',');
-                _putchar(' ');
             }
 
             if (result < 10) {
+                _putchar(' ');
                 _putchar(result + '0');
             } else {
+                _putchar(' ');
                 _putchar(result / 10 + '0');
                 _putchar(result % 10 + '0');
             }
