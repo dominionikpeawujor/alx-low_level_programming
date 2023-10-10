ee12c7ee3e3f3c995ae2915b4d403a45a396678c
Dominion Ikpeawujor <51343512+dominionikpeawujor@users.noreply.github.com>
2023-10-11 00:36:10 +0100

    Create 9-main.c

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - Print the multiplication table for 0 through 9.
+ *
+ * Description: This function calculates and prints the products of numbers from
+ * 0 to 9, forming a multiplication table. It ensures proper formatting to align
+ * the results in a tabular form.
+ *
+ * Return: None (void)
+ */
+void times_table(void)
+{
+    int row, column, result;
+
+    for (row = 0; row <= 9; row++) {
+        for (column = 0; column <= 9; column++) {
+            result = row * column;
+
+            if (column == 0) {
+                _putchar('0');
+            } else {
+                if (result < 10) {
+                    _putchar(' ');
+                }
+                _putchar((result / 10) + '0');
+                _putchar((result % 10) + '0');
+            }
+
+            if (column < 9) {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+        _putchar('\n');
+    }
+}
+
