d25073b317b48ecc9866e02e64c5823040d0fd30
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-10-10 15:09:40 -0700

    Update file

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,8 +4,10 @@
 /**
  * _islower - Check if a character is a lowercase letter
  *
- * Description: This function takes an ASCII value (c) as input and checks if it
- * represents a lowercase letter in the English alphabet. It returns 1 if the
+ * Description:
+ * This function takes an ASCII value (c) as input and checks if it
+ * represents a lowercase letter in the English alphabet.
+ * It returns 1 if the
  * character is a lowercase letter and 0 otherwise.
  *
  * @c: The ASCII value to be checked
