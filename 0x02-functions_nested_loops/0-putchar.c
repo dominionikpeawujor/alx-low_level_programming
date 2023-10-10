11dc0385535dcf1d663e18f8d0e7845984bb354e
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-10-10 14:30:43 -0700

    Add manin description to 0-putchar.c file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This is the main function where the program starts
+ * execution. It prints "_putchar" and returns 0 to indicate successful
+ * execution.
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	printf("_putchar\n");
