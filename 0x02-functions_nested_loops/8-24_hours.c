45acc62e691365390a42a2151aad3186d5ff21dc
Dominion Ikpeawujor <51343512+dominionikpeawujor@users.noreply.github.com>
2023-10-11 00:27:14 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * jack_bauer - Print all the minutes in a day
+ *
+ * Description: This function generates and prints all the minutes in a 24-hour
+ * day in the "HH:MM" format. It goes through all the possible hours (00 to 23)
+ * and minutes (00 to 59) combinations and prints them to the standard output.
+ *
+ * Return: None (void)
+ */
 void jack_bauer(void)
 {
-	int i, j, hours, mins;
-
-	for (i = 0; i <24; i++)
+	int hours, mins;
+	for (hours = 0; hours < 24; hours++)
 	{
-		for (j = 0; j < 60; j++)
+		for (mins = 0; mins < 60; mins++)
 		{
-			(i < 10)? hours = '0' + i : hours = i;
-			(j < 10)? mins = '0' + j : mins = j;
-			_putchar(hours);
-			_putchar(':');
-			_putchar(mins);
-		}
+			char hours_tens = (hours < 10) ? '0' : '0' + (hours / 10);
+			char hours_ones = '0' + (hours % 10);
+			char mins_tens = (mins < 10) ? '0' : '0' + (mins / 10);
+			char mins_ones = '0' + (mins % 10);
+			putchar(hours_tens);
+			putchar(hours_ones);
+			putchar(':');
+			putchar(mins_tens);
+			putchar(mins_ones);
+			putchar('\n');
 	}
+    }
 }
