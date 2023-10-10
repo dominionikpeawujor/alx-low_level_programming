0c98018211e568c44e0d16482e2f0a0d6760d644
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-10-10 15:15:02 -0700

    Update file

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * _isalpha - Check if a character is an 
+ * alphabetic character
+ *
+ * Description: This function takes an ASCII value (c) as input and checks if it
+ * represents an alphabetic character (either uppercase or lowercase)
+ * in the English alphabet. It returns 1 if the character is alphabetic 
+ * and 0 otherwise.
+ *
+ * @c: The ASCII value to be checked
+ *
+ * Return: 1 if the character is alphabetic, 0 otherwise
+ */
 int _isalpha(int c)
 {
 	if (c >= 65 && c <= 123)
