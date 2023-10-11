03ee97873d3cba97b2965a0f5d164236181c5829
Dominion Ikpeawujor <51343512+dominionikpeawujor@users.noreply.github.com>
2023-10-11 01:37:20 +0100

    Create 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_times_table - Print the n times table, starting with 0.
+ *
+ * Description: This function takes an integer 
+ * 'n' as input and prints the
+ * multiplication table for 'n'. The table starts
+ * from 0 and goes up to 'n * 15'
+ * in the specified format. It ensures proper formatting and alignment.
+ * If 'n' is greater than 15 or less than 0,
+ * the function does not print anything.
+ *
+ * @n: The number for which the multiplication table is generated
+ *
+ * Return: None (void)
+ */
+void print_times_table(int n)
+{
+	if (n > 15 || n < 0)
+		return;
+	int row, column, result;
+	
+	for (row = 0; row <= n; row++)
+	{
+		for (column = 0; column <= n; column++)
+		{
+			result = row * column;
+			if (column == 0)
+			{
+				_putchar('0');
+			}
+			else
+			{
+				if (result < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else if (result < 100)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+				
+				int temp = result;
+				int digit;
+				
+				while (temp > 0)
+				{
+					digit = temp % 10;
+					_putchar(digit + '0');
+					temp /= 10;
+
+				}
+			}
+			if (column < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
