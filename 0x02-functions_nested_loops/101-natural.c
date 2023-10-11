4c98d78f1f409e5d8e97f31d89528c4ca59dfa42
Dominion Ikpeawujor <51343512+dominionikpeawujor@users.noreply.github.com>
2023-10-11 01:58:17 +0100

    Create 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program calculates 
+ * and prints the sum of all the multiples
+ * of 3 or 5 below 1024. It uses a loop to
+ * iterate through the numbers from 1 to
+ * 1023 and checks if each number is a multiple
+ * of 3 or 5. If it is, it adds the
+ * number to the sum. Finally, it prints the calculated sum.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int limit = 1024;
+	int sum = 0;
+	
+	for (int i = 1; i < limit; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return 0;
+}
