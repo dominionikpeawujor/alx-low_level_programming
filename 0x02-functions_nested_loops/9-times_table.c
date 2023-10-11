2479f9967507a9fba648b5d7acac575fad2bd440
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-10-10 17:12:05 -0700

    Update file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,7 @@
  *
  * Description: This function calculates
  * and prints the products of numbers from
- * 0 to 9, forming a multiplication table. 
+ * 0 to 9, forming a multiplication table.
  * It ensures proper formatting to align
  * the results in a tabular form.
  *
@@ -17,30 +17,30 @@
  */
 void times_table(void)
 {
-        int row, column, result;
-        
-        for (row = 0; row <= 9; row++)
-        {
-                for (column = 0; column <= 9; column++)
-                {
-                        result = row * column;
-                        if (column > 0)
-                        {
-                                _putchar(',');
-                                _putchar(' ');
-                        }
-                        if (result < 10)
-                        {
-                                _putchar(' ');
-                                _putchar(' ');
-                                _putchar(result + '0');
-                        }
-                        else
-                        {
-                                _putchar(result / 10 + '0');
-                                _putchar(result % 10 + '0');
-                        }
-                }
-                _putchar('\n');
-        }
+	int row, column, result;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			result = row * column;
+			if (column > 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (result < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
