b6ff04c737b67dbbb17e9c244b3779334f1ba6a1
Dominion Ikpeawujor <51343512+dominionikpeawujor@users.noreply.github.com>
2023-10-11 01:18:03 +0100

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - Print the last digit of an integer
+ *
+ * Description: This function takes an integer (n)
+ * as input and computes and
+ * returns the last digit of that integer.
+ * The last digit is the remainder of
+ * the integer when divided by 10.
+ *
+ * @n: The integer from which the last digit is extracted
+ *
+ * Return: The last digit of the input integer (an integer between 0 and 9)
+ */
+int add(int a, int b)
+{
+        return (a + b);
+}
