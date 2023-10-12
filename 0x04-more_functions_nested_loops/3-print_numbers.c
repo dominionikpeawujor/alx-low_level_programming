43ae7c1dbb096e8ea57cddf7faa29f3522b06d92
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-10-12 16:31:54 -0700

    Add Print Numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: This function does not return a value.
+ */
+void print_numbers(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		_putchar("%d", i);
+	}
+	return (0);
+}
