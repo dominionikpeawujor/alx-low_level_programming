07a071cad545be952655a884977bff3dfc49253d
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-10-13 06:41:43 -0700

    Update file

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,7 +8,7 @@
  */
 void print_number(int n)
 {
- 	if (n < 0)
+	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
@@ -16,6 +16,6 @@ void print_number(int n)
 	if (n / 10 != 0)
 	{
 		print_number(n / 10);
-    	}
+	}
 	_putchar(n % 10 + '0');
 }
