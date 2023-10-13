8f6c03dbf406639f256214f4de275b45d0463ef9
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-10-13 06:28:19 -0700

    Create new file

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	int i;
+	long int number;
+	number = 612852475143;
+
+	for (i = 2; i < number; i++)
+	{
+		while (number % i == 0)
+		{
+			number %= i;
+		}
+	}
+	printf("%ld", number);
+	return (0);
+}
