029832b0528f6afe9fe664b58caf52c22f1f3981
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-10-16 15:08:25 -0700

    Create new file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+/**
+ * _strlen - Returns the length of a string.
+ * @s: A pointer to the string.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	if (s != NULL)
+	{
+		while (*s)
+		{
+			length++;
+			s++;
+		}
+	}
+
+	return length;
+}
+
