9f7f43c0530dffcfc8238b752ad023517caa3a1b
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-10-16 23:01:16 +0100

    Edir file 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+/**
+ * reset_to_98 - Update the value pointed to by an integer pointer to 98.
+ * @n: A pointer to an integer that we want to set to 98.
+ */
 void reset_to_98(int *n)
 {
-    n = 98;
+    *n = 98;
 }
