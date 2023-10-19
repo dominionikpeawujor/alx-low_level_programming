b65a9970d0857fcc983b10a909b1f46ff7470730
Dominion Ikpeawujor <51343512+dominionikpeawujor@users.noreply.github.com>
2023-10-19 21:22:17 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,12 @@
  *   a negative value if s1 is less than s2,
  *   a positive value if s1 is greater than s2.
  */
-int _strcmp(char *s1, char *s2) {
-    while (*s1 && (*s1 == *s2)) {
-        s1++;
-        s2++;
+int _strcmp(char *s1, char *s2) 
+{
+	while (*s1 && (*s1 == *s2)) 
+	{
+		s1++;
+		s2++;
     }
-
     return *(unsigned char *)s1 - *(unsigned char *)s2;
 }
-
