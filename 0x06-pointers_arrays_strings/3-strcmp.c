1b60a3793f02b0a6f9f29e13f0da22ad656204e6
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-10-19 13:19:22 -0700

    Update file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * _strcmp - Compares two strings.
+ * @s1: First string.
+ * @s2: Second string.
+ *
+ * Returns:
+ *   0 if the strings are equal,
+ *   a negative value if s1 is less than s2,
+ *   a positive value if s1 is greater than s2.
+ */
+int _strcmp(char *s1, char *s2) {
+    while (*s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+
+    return *(unsigned char *)s1 - *(unsigned char *)s2;
+}
+
