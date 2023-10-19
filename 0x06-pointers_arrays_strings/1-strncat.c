4cdd69a956f07ecf4339119ef6d3bdf399efabea
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-10-19 13:04:23 -0700

    Update file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * _strncat - Concatenates two strings, limiting the number of characters from src.
+ * @dest: The destination string.
+ * @src: The source string to append to dest.
+ * @n: The maximum number of characters to concatenate from src.
+ *
+ * This function appends at most n bytes from the src string to the dest string.
+ * The src string does not need to be null-terminated if it contains n or more bytes.
+ * The function adds a terminating null byte to the end of dest.
+ *
+ * Returns: A pointer to the resulting string dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *result = dest;
+
+	while (*dest)
+	{
+		dest++;
+	}
+	while (*src && n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+	*dest = '\0';
+
+	return (result);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,3 @@
 int _putchar(char c);
 char *_strcat(char *dest, const char *src);
-
+char *_strncat(char *dest, char *src, int n);
