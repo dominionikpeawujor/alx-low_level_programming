b2639782ca44a094911f6dd625df5e89cf0dd442
root <root@Desktop.localdomain>
2023-10-26 17:06:42 +0100

    Update file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,5 +6,5 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_recursive(int n, int divisor);
-int check_palindrome(char *s, int start, int end);
+int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
