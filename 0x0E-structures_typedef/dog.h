d68bd76a0543b5627fa317c90d1ec3eaad1ccfcb
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-11-06 13:10:32 -0800

    Initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * struct dog - Structure for 
+ * information about a dog.
+ * @name: Name of the dog (string).
+ * @age: Age of the dog (float).
+ * @owner: Owner of the dog (string).
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
