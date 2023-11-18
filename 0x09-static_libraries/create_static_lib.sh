18cc1198fe7412f34923b40a11e14bb6a9eec6ae
root <root@Desktop.localdomain>
2023-11-18 11:25:20 +0100

    Add new files

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-gcc -c -Wall -Werror -Wextra *.c
-ar -rc liball.a *.o
-ranlib liball.a
+# Compile each .c file into a corresponding .o file
+for file in *.c; do
+    if [ -f "$file" ]; then
+        gcc -c "$file" -o "${file%.c}.o"
+    fi
+done
+
+# Create the static library (liball.a) from all the .o files
+ar rcs liball.a *.o
+
+# Clean up the temporary .o files
+rm -f *.o
+
+echo "Static library liball.a created successfully."
